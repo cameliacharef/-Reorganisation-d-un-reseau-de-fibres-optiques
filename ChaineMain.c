b4cebe1aec6aaac036e7d804115892b8333d1b0e
camelia <126248163+cameliacharef@users.noreply.github.com>
2024-03-02 19:42:45 +0100

    Update ChaineMain.c

--- ChaineMain.c
+++ ChaineMain.c
@@ -4,17 +4,31 @@
 
 #include "Chaine.h"
 
-int main (){
-
+int main() {
     FILE * fichier = fopen("00014_burma.cha", "r"); 
-
-        Chaines *chaines = lectureChaines(fichier);
-        fclose(fichier);
-
-        // Affichez les chaines
-        printf("Chaines lues du fichier :\n");
-        afficher_Chaines(chaines);
-        
+    if (fichier == NULL) {
+        printf("Error opening file\n");
+        return 1;
+    }
+    Chaines *chaines = lectureChaines(fichier);
     
+    fclose(fichier);
+
+    if(argc != 2){
+        printf("Il faut 1 argument : nom_du_fichier\n");
+        return 0; 
+    }
+
+    fichier = fopen(argv[1], "w"); 
+    if (fichier == NULL) {
+        printf("Error opening file\n");
+        return 1;
+    }
+    ecrireChaines(chaines, fichier);
+
+    fclose(fichier);
+    liberer_Chaines(chaines);
+
     return 0;
 }
+
