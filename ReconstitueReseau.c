150421464f43ae74afbee3f4485d94c4dacdb4b7
lisarincky <161413723+lisarincky@users.noreply.github.com>
2024-03-03 14:24:41 +0100

    Update ReconstitueReseau.c

--- ReconstitueReseau.c
+++ ReconstitueReseau.c
@@ -6,5 +6,26 @@
 #include "SVGwriter.h"
 
 int main(){
+  if(argc != 3){
+        printf("Il faut 2 arguments : le nom du fichier et le numéro de l'action.\n");
+        return 0;
+  }
+  
+  FILE * fichier = fopen("00014_burma.cha", "r"); 
+  if (fichier == NULL) {
+      printf("Error opening file\n");
+      return 1;
+  }
+  Chaines *chaines = lectureChaines(fichier);
+
+  if(argv[1]==1){
+
+  }else if(argv[1]==2){    
+    Reseau* R = reconstitueReseauListe(chaines);
+  }else{
+    printf("Saisissez un autre nombre.\n");
+  }
+
+  fclose(fichier);
   return 0;
 }
