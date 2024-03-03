2887a7b145fbb2057e621676e3cedcb3040a755c
lisarincky <161413723+lisarincky@users.noreply.github.com>
2024-03-03 19:45:34 +0100

    Update ReconstitueReseau.c

--- ReconstitueReseau.c
+++ ReconstitueReseau.c
@@ -11,24 +11,35 @@ int main(int argc, char* argv[]){
         return 0;
   }
   
-  FILE * fichier = fopen("00014_burma.cha", "r"); 
-  if (fichier == NULL) {
+  FILE * fl = fopen("00014_burma.cha", "r"); 
+  if (fl == NULL) {
       printf("Error opening file\n");
       return 1;
   }
-  Chaines *chaines = lectureChaines(fichier);
+  Chaines *chaines = lectureChaines(fl);
 
   if(atoi(argv[2])==1){
-    Reseau* R = NULL;
-    Noeud* N1 = rechercheCreeNoeudListe(R,0,3);
-    Noeud* N2 = rechercheCreeNoeudListe(R,0,3);
-    Noeud* N3 = rechercheCreeNoeudListe(R,10,3);
-  }else if(atoi(argv[2])==2){    
     Reseau* R = reconstitueReseauListe(chaines);
+    Noeud* N1 = rechercheCreeNoeudListe(R,10.00,31.23);
+    Noeud* N2 = rechercheCreeNoeudListe(R,10.00,31.23);
+    Noeud* N3 = rechercheCreeNoeudListe(R,20.09,92.54);
+
+    int nbC = nbCommodites(R);
+    int nbL = nbLiaisons(R);
+    printf("Nombre de commodités : %d\nNombre de liaisons : %d\n",nbC,nbL);
+
+    FILE* fe = fopen("test_exo3.res","w");
+    ecrireReseau(R,fe);
+    //afficheReseauSVG(R,???);
+    fclose(fe);
+  }else if(atoi(argv[2])==2){    
+    printf("Méthode non encore mise en place\n");
+  }else if(atoi(argv[2])==3){    
+    printf("Méthode non encore mise en place\n");
   }else{
     printf("Saisissez un autre nombre.\n");
   }
 
-  fclose(fichier);
+  fclose(fl);
   return 0;
 }
