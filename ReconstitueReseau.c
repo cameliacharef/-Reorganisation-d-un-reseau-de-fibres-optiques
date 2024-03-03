b33015cb6a06530c1c52cd79fef437c09a6f6153
lisarincky <161413723+lisarincky@users.noreply.github.com>
2024-03-03 14:50:40 +0100

    Update ReconstitueReseau.c

--- ReconstitueReseau.c
+++ ReconstitueReseau.c
@@ -5,7 +5,7 @@
 #include "Reseau.h"
 #include "SVGwriter.h"
 
-int main(){
+int main(int argc, char* argv[]){
   if(argc != 3){
         printf("Il faut 2 arguments : le nom du fichier et le numéro de l'action.\n");
         return 0;
@@ -18,12 +18,12 @@ int main(){
   }
   Chaines *chaines = lectureChaines(fichier);
 
-  if(argv[1]==1){
+  if(atoi(argv[2])==1){
     Reseau* R = NULL;
     Noeud* N1 = rechercheCreeNoeudListe(R,0,3);
     Noeud* N2 = rechercheCreeNoeudListe(R,0,3);
     Noeud* N3 = rechercheCreeNoeudListe(R,10,3);
-  }else if(argv[1]==2){    
+  }else if(atoi(argv[2])==2){    
     Reseau* R = reconstitueReseauListe(chaines);
   }else{
     printf("Saisissez un autre nombre.\n");
