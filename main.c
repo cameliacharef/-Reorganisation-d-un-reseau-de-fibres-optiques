b084398f9cbc25ea34744b0f388784a1f125ef6f
lisarincky <161413723+lisarincky@users.noreply.github.com>
2024-04-13 18:41:01 +0000

    Une question faite

--- main.c
+++ main.c
@@ -1,48 +1,93 @@
 #include <stdio.h> 
 #include <stdlib.h> 
 #include <assert.h>
+#include <time.h>
 #include "Chaine.h"
 #include "Reseau.h"
 #include "SVGwriter.h"
 #include "Hachage.h"
 #include "ArbreQuat.h"
 
-int main(){
-	//Initialisation des variables de temps de calcul
-	clock_t tpsInitLC;
-	clock_t tpsFinLC;
-	clock_t tpsInitH;
-	clock_t tpsFinH;
-	clock_t tpsInitAQ;
-	clock_t tpsFinAQ;
+int main(int argc , char *argv[]){
+	//Ouverture du fichier de lecture
+	if(argc != 2){
+        printf("Il faut 1 argument : le nom du fichier .cha.\n"); // "00014_burma.cha"
+        return 0;
+    }
+    char *nomfic = argv[1];
+
+    FILE * fl = fopen(nomfic, "r");
+    if (fl == NULL) {
+        printf("Error opening file\n");
+        return 1;
+    }
+
+	//Ouverture du fichier d'écriture
+	FILE * fe = fopen("comparaisonTpsCalcul", "w");
+
+	//Initialisation des variables et fermeture du fichier de lecture
+	clock_t tpsInit;
+	clock_t tpsFin;
+	double tpsCalcul;
+	Chaines *chaines = lectureChaines(fl);
+	fclose(fl);
 
 	//Lancement des trois programmes et calcul des temps de calcul
+	fprintf(fe,"Comparaisons de calcul :\n");
 
 	//1.Listes chaînées
-	tpsInitLC = clock();
-	/*...*/
-	tpsFinLC = clock();
+	tpsInit = clock();
+	Reseau* reseauLC = reconstitueReseauListe(chaines);
+	tpsFin = clock();
 
-	//2.Tables de hachage
-	tpsInitH = clock();
-	/*...*/
-	tpsFinH = clock();
+	liberer_Reseau(reseauLC);
+
+	tpsCalcul = 1000*((double)(tpsFin - tpsInit)) / CLOCKS_PER_SEC;
+	fprintf(fe,"Temps de calcul pour les listes chaînées : %.4f millisecondes\n",tpsCalcul);
+
+	//2.Tables de hachage :
+	//- de taille 10 :
+	tpsInit = clock();
+	Reseau* reseauH10 = reconstitueReseauHachage(chaines,10);
+	tpsFin = clock();
+
+	liberer_Reseau(reseauH10);
+
+	tpsCalcul = 1000*((double)(tpsFin - tpsInit)) / CLOCKS_PER_SEC;
+	fprintf(fe,"Temps de calcul pour les tables de hachage de taille 10 : %.4f millisecondes\n",tpsCalcul);
+
+	//- de taille 100 :
+	tpsInit = clock();
+	Reseau* reseauH100 = reconstitueReseauHachage(chaines,100);
+	tpsFin = clock();
+
+	liberer_Reseau(reseauH100);
+
+	tpsCalcul = 1000*((double)(tpsFin - tpsInit)) / CLOCKS_PER_SEC;
+	fprintf(fe,"Temps de calcul pour les tables de hachage de taille 100 : %.4f millisecondes\n",tpsCalcul);
+
+	//- de taille 1000 :
+	tpsInit = clock();
+	Reseau* reseauH1000 = reconstitueReseauHachage(chaines,1000);
+	tpsFin = clock();
+
+	liberer_Reseau(reseauH1000);
+
+	tpsCalcul = 1000*((double)(tpsFin - tpsInit)) / CLOCKS_PER_SEC;
+	fprintf(fe,"Temps de calcul pour les tables de hachage de taille 1000 : %.4f millisecondes\n",tpsCalcul);
 
 	//3.Arbres quaternaires
-	tpsInitAQ = clock();
-	/*...*/
-	tpsFinAQ = clock();
+	tpsInit = clock();
+	Reseau* reseauAQ = reconstitueReseauArbre(chaines);
+	tpsFin = clock();
+	liberer_Reseau(reseauAQ);
 
-	//Comparaisons des temps de calcul pour les trois programmes
-	double tpsCalculLC = ((double)(tpsFinLC - tpsInitLC)) / CLOCKS_PER_SEC;
-	double tpsCalculH = ((double)(tpsFinH - tpsInitH)) / CLOCKS_PER_SEC;
-	double tpsCalcuAQ = ((double)(tpsFinAQ - tpsInitAQ)) / CLOCKS_PER_SEC;
+	tpsCalcul = 1000*((double)(tpsFin - tpsInit)) / CLOCKS_PER_SEC;
+	fprintf(fe,"Temps de calcul pour les arbres quaternaires : %.4f millisecondes\n",tpsCalcul);
 
-	/*...*/
-
-	//Libération des structures de données
-	
-	/*...*/
+	//Fermeture du fichier d'écriture et libération des chaines
+	fclose(fe);
+	liberer_Chaines(chaines);
 
 	return 0;
 }
